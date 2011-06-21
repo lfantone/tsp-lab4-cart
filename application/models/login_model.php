8d3ac02b667a814bbcfb20bd99651d9c3416b51f
Luciano Fantone <intheflesh@intheflesh-desktop.(none)>
2011-06-21 18:18:18 -0300

    modificado para funcionar con nuevos nombres

--- application/models/login_model.php
+++ application/models/login_model.php
@@ -6,7 +6,6 @@ class Login_model extends CI_Model {
 	}
 	
 	function CheckUserAndPass($email,$password) { 
-		$password = $this->encrypt->decode($password);
 		$query = $this->db->where('mail',$email);
 		$query = $this->db->where('password',$password);
 		$query = $this->db->get('usuarios');
