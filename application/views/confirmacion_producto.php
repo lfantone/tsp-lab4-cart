57438a983e439d6c13e5c89f7745a098501908ad
Luciano Fantone <intheflesh@intheflesh-desktop.(none)>
2011-06-21 18:21:01 -0300

    borrar mas adelante

---
+++ application/views/confirmacion_producto.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Confimaci&oacute;n</title>
+</head>
+<body>
+
+<h3>SE HA ENVIADO CORRECTAMENTE!</h3>
+
+<p><?php echo anchor('alta', 'IR A LA PAGINA PRINCIPAL'); ?></p>
+
+</body>
+</html>
