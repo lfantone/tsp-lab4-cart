32f40aef61ae46eb31decd37c931bbea6bfd4672
Luciano Fantone <intheflesh@intheflesh-desktop.(none)>
2011-06-21 18:17:51 -0300

    test conn

--- application/config/database.php
+++ application/config/database.php
@@ -40,7 +40,7 @@
 
 $active_group = 'default';
 $active_record = TRUE;
-
+/*
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '';
@@ -56,6 +56,21 @@ $db['default']['dbcollat'] = 'utf8_general_ci';
 $db['default']['swap_pre'] = '';
 $db['default']['autoinit'] = TRUE;
 $db['default']['stricton'] = FALSE;
-
+*/
+$db['default']['hostname'] = 'cringer.contenta';
+$db['default']['username'] = 'contenta';
+$db['default']['password'] = 'contenta';
+$db['default']['database'] = 'gaming-desa';
+$db['default']['dbdriver'] = 'postgre';
+$db['default']['dbprefix'] = '';
+$db['default']['pconnect'] = TRUE;
+$db['default']['db_debug'] = TRUE;
+$db['default']['cache_on'] = FALSE;
+$db['default']['cachedir'] = '';
+$db['default']['char_set'] = 'utf8';
+$db['default']['dbcollat'] = 'utf8_general_ci';
+$db['default']['swap_pre'] = '';
+$db['default']['autoinit'] = TRUE;	
+$db['default']['stricton'] = FALSE;
 /* End of file database.php */
 /* Location: ./application/config/database.php */
