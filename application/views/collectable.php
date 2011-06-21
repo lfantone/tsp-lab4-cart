62848715f7760ceca153dbfccd932a7c87f12773
Luciano Fantone <intheflesh@intheflesh-desktop.(none)>
2011-06-21 16:05:36 -0300

    git pull me rompio todo.

--- application/views/collectable.php
+++ application/views/collectable.php
@@ -12,8 +12,7 @@
    	</thead>  
    	<tbody>
 	   	<?php foreach ($collectable as $c) {?>
-			<tr <?php if(alternator('0', '1')) { echo 'class="alt"'; }?>>	   			
-<<<<<<< HEAD
+			<tr <?php if(alternator('0', '1')) { echo 'class="alt"'; }?>>
 				<td><?=character_limiter($c['collectable_name'], 8, '')?></td>
 			    <td><?=$c['base_price']?></td>
 			    <td><?=$c['base_price']-$c['base_price']*0.10?></td>
@@ -21,8 +20,7 @@
 			    <td><?php print_r($c['id_collectable']);?></td>
 			    <td><?php echo form_open('cart/add_cart_item', array('name'=>'a'));
 	   					  echo form_input('quantity', '1', 'maxlength="2"');	  	  
-			    		  echo form_hidden('id_collectable', $c['id_collectable']);?></td>   						
-=======
+			    		  echo form_hidden('id_collectable', $c['id_collectable']);?></td>
 				<td><?=character_limiter($c['descripcion'], 8, '')?></td>
 			    <td><?=$c['precio']?></td>
 			    <td><?=$c['precio']-$c['precio']*0.10?></td>
@@ -30,8 +28,7 @@
 			    <td><?php print_r($c['id_producto']);?></td>
 			    <td><?php echo form_open('cart/add_cart_item', array('name'=>'a'));
 	   					  echo form_input('quantity', '1', 'maxlength="2"');	  	  
-			    		  echo form_hidden('id_producto', $c['id_producto']);?></td>   						
->>>>>>> de7319b30ab1fef9cd8257284caac78d25dd4aed
+			    		  echo form_hidden('id_producto', $c['id_producto']);?></td>
 			    <td><?php echo form_submit('add', 'Agregar');?></td> 		    		
 		   			<?php echo form_close();?>	   			
 		    </tr>  					
