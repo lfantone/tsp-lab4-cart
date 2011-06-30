0cd214c97c04ff37a7b210e61b0de36752992c48
Luciano Fantone <lucianoafantone@gmail.com>
2011-06-30 12:20:02 -0300

    nuevo banner y banner cpanel

---
+++ application/views/banner.php
@@ -0,0 +1,10 @@
+	<div id="dolphincontainer">
+	  <div id="dolphinnav">
+	  <ul>
+	    <li><?php echo anchor('cart', 'Home'); ?></li>
+		<li><?php echo anchor('cart', 'Productos'); ?></li>
+		<li><?php echo anchor('perfil', 'Mi perfil'); ?></li>
+		<li><?php echo anchor('cart', 'Mi carrito'); ?></li>
+	  </ul>
+	  </div>
+	</div>
---
+++ application/views/banner_admin.php
@@ -0,0 +1,8 @@
+	<div id="dolphincontainer">
+	  <div id="dolphinnav">
+	  <ul>
+	    <li><?php echo anchor('cpanel', 'Home'); ?></li>
+		<li><?php echo anchor('exit', 'Log Out'); ?></li>		
+	  </ul>
+	  </div>
+	</div>
