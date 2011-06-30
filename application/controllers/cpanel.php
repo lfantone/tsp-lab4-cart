fceba99690dc773039742a5e8ecc11ba0acffaae
Luciano Fantone <lucianoafantone@gmail.com>
2011-06-30 12:16:09 -0300

    Admin Control Panel

---
+++ application/controllers/cpanel.php
@@ -0,0 +1,40 @@
+<?php
+	class CPanel extends CI_Controller {
+		public function __construct() {
+			parent::__construct();
+		}
+	
+		public function index() {
+			$data['link'] = array(
+                  	'1' => 'Producto',
+                  	'2' => 'Proveedor',
+                  	'3' => 'Categor&iacute;a');
+			$data['opciones'] = array(                  	
+                  	'1' => 'Baja',
+                  	'2' => 'Modificaciones');			
+			$this->load->view('cpanel', $data);			
+		}		
+		
+		public function redireccion() {			
+			if($this->input->post('link')) {
+				if($this->input->post('link') == 1) {
+					redirect('alta');				
+				}
+				if($this->input->post('link') == 2) {
+					$this->load->view('alta_proveedor');
+				}
+				if($this->input->post('link') == 3) {			
+					$this->load->view('alta_categoria');
+				}								
+			}
+			if ($this->input->post('opciones')) {
+				if($this->input->post('opciones') == 1) {
+					redirect('baja');				
+				}
+				if($this->input->post('opciones') == 2) {
+					redirect('modificacion');
+				}
+			}
+		}
+	}
+?>
---
+++ application/views/cpanel.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CPanel</title>	
+	<link href="<?=base_url()?>assets/css/structure.css" rel="stylesheet" type="text/css" />
+	<link href="<?=base_url()?>assets/css/form.css" rel="stylesheet" type="text/css" />
+	<link href="<?=base_url()?>assets/css/banner.css" rel="stylesheet" type="text/css" />
+</head>
+<body id="public">
+	<?php $this->view('banner_admin.php');?>
+	<div id="container">
+		<?php echo form_open('cpanel/redireccion', 'class="form_new_producto"');?>
+		<header id="header" class="info">
+			<h2>CPanel</h2>
+		</header>
+		<ul>	
+			<li class="item1">
+				<div>
+					<?php echo form_label('Para dar de alta, seleccionar una opci&oacute;n', 'link');?>
+					<?php echo form_dropdown('link', $link);?>					
+				</div>				
+			</li>
+			<li class="button">
+				<div>
+					<?php echo form_submit('ir', 'Ir', 'class="btSubmit"');?>
+				</div>
+				<?php echo form_close();?>			
+			</li>	
+			<li class="item2">
+				<?php echo form_open('cpanel/redireccion', 'class="form_new_producto"');?>
+				<div>
+					<?php echo form_label('Tipo de consulta', 'opciones')?>
+					<?php echo form_dropdown('opciones', $opciones);?>
+				</div>
+			</li>
+			<li class="button">
+				<div>
+					<?php echo form_submit('ir', 'Ir', 'class="btSubmit"');?>
+				</div>
+				<?php echo form_close();?>
+			</li>			
+		</ul>
+	</div>	
+</body>
+</html>						
+						
