cf3817432415ca0e2f980c7a18bb7b324da2af18
Luciano Fantone <lucianoafantone@gmail.com>
2011-06-30 12:08:45 -0300

    corregidas traducciones y agregadas las que faltaban

--- application/language/spanish/form_validation_lang.php
+++ application/language/spanish/form_validation_lang.php
@@ -1,21 +1,25 @@
 <?php
 
-$lang['required'] = 'El campo %s es obligatorio.';
-$lang['isset'] = 'El campo %s debe contener un valor.';
-$lang['valid_email'] = 'El campo %s debe contener una direccion valida';
-$lang['valid_emails'] = 'El campo %s debe contener todas las direcciones de email v';
-$lang['valid_url'] = 'El campo %s debe contener una URL v';
-$lang['valid_ip'] = 'El campo %s debe contener una IP v';
-$lang['min_length'] = 'El campo %s debe tener al menos %s caracteres de longitud.';
-$lang['max_length'] = 'El campo %s no puede exceder los %s caracteres de longitud.';
-$lang['exact_length'] = 'El campo %s debe tener exactamente %s caracteres de longitud.';
-$lang['alpha'] = 'El campo %s debe contener s';
-$lang['alpha_numeric'] = 'El campo %s debe contener s';
-$lang['alpha_dash'] = 'El campo %s debe contener s';
-$lang['numeric'] = 'El campo %s debe contener s';
-$lang['is_numeric'] = 'El campo %s debe contener s';
-$lang['integer'] = 'El campo %s debe contener un n';
-$lang['matches'] = 'El campo %s no coincide con el campo %s.';
-$lang['is_natural'] = 'El campo %s debe contener s';
-$lang['is_natural_no_zero'] = 'El campo %s debe contener un n';
+$lang['required'] 			= 'El campo %s es obligatorio.';
+$lang['isset'] 				= 'El campo %s debe contener un valor.';
+$lang['valid_email']		= 'El campo %s debe contener una direcci&oacute;n v&aacute;lida.';
+$lang['valid_emails'] 		= 'El campo %s debe contener todas las direcciones de email v&aacute;lidas.';
+$lang['valid_url'] 			= 'El campo %s debe contener una URL v&aacute;lida.';
+$lang['valid_ip'] 			= 'El campo %s debe contener una IP v&aacute;lida.';
+$lang['min_length'] 		= 'El campo %s debe tener al menos %s caracteres de longitud.';
+$lang['max_length'] 		= 'El campo %s no puede exceder los %s caracteres de longitud.';
+$lang['exact_length'] 		= 'El campo %s debe tener exactamente %s caracteres de longitud.';
+$lang['alpha'] 				= 'El campo %s debe contener s&oacute;lo caracteres alfab&eacute;ticos.';
+$lang['alpha_numeric'] 		= 'El campo %s debe contener s&oacute;lo caracteres alfanum&eacute;ricos.';
+$lang['alpha_numeric_spaces'] 		= 'El campo %s debe contener s&oacute;lo caracteres alfanum&eacute;ricos y espacios.';
+$lang['alpha_dash'] 		= 'El campo %s debe contener s&oacute;lo caracteres alfanume&eacute;ricos, _ y /.';
+$lang['numeric'] 			= 'El campo %s debe contener s&oacute;lo n&uacute;meros.';
+$lang['is_numeric'] 		= 'El campo %s debe contener s&oacute;lo caractere num&eacute;ricos.';
+$lang['integer']			= 'El campo %s debe contener un n&uacute;mero entero';
+$lang['matches'] 			= 'El campo %s no coincide con el campo %s.';
+$lang['is_natural'] 		= 'El campo %s debe contener s&oacute;lo n&uacute;meros positivos.';
+$lang['is_natural_no_zero'] = 'El campo %s debe contener un n&uacute;mero mayor a cero.';
+$lang['decimal']			= "El campo %s debe contener un n&uacute;mero decimal.";
+$lang['less_than']			= "El campo %s debe contener un n&uacute;mero menor a %s.";
+$lang['greater_than']		= "El campo %s debe contener un n&uacute;mero mayor a %s.";
 ?>
