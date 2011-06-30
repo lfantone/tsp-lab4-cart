31ba2e4c9e0fc87008e18f3fdb3955567d5a52de
Luciano Fantone <lucianoafantone@gmail.com>
2011-06-30 12:08:16 -0300

    nueva set_rule + md5

--- application/controllers/registro.php
+++ application/controllers/registro.php
@@ -1,50 +1,47 @@
 <?php
-
-class registro extends CI_Controller {
-
-	public function __construct() {
-		parent::__construct();
-		$this->load->model('registro_model');
-		$this->load->library('form_validation');
-		$this->load->helper(array('form', 'url', 'security'));		
-	}
+	class Registro extends CI_Controller {
 	
-	public function index() {		
-		$this->load->view('registrousuario');	
-	}
-	
-	public function new_user() {
-		$this->form_validation->set_rules('firstname', 'Nombre', 'trim|required|min_length[2]|max_length[30]|alpha|xss_clean');
-		$this->form_validation->set_rules('lastname', 'Apellido', 'trim|required|min_length[2]|max_length[30]|alpha');
-		$this->form_validation->set_rules('password', 'Contraseña', 'required|min_length[8]|max_length[30]');
-		$this->form_validation->set_rules('passconf', 'Confirme Contraseña', 'required|matches[password]');
-		$this->form_validation->set_rules('email', 'Email', 'trim|required|valid_email|max_length[50]|callback_username_check');
-		$this->form_validation->set_rules('address', 'Dirección', 'trim|required|min_length[2]|max_length[50]|alpha_numeric');
-		$this->form_validation->set_rules('address_number', 'Número de Dirección', 'trim|required|min_length[1]|max_length[10]|is_natural_no_zero');
-		$this->form_validation->set_rules('city', 'Localidad', 'trim|required|min_length[2]|max_length[50]|alpha_numeric');
+		public function __construct() {
+			parent::__construct();
+			$this->load->model('registro_model');		
+		}
 		
-		if ($this->form_validation->run() == FALSE) {
-			$this->load->view('registrousuario');
-		} else {			/* VALIDACIONES */
-			if ($this->registro_model->insert_newUser($this->input->post('firstname'), $this->input->post('lastname'), $this->encrypt->encode($this->input->post('password')), $this->input->post('email'), $this->input->post('address'), $this->input->post('address_number'), $this->input->post('city'))) {
-				$this->load->view('registrocorrecto');
-			} else {
-				$this->load->view('registroErr');
-			}	
+		public function index() {		
+			$this->load->view('registrousuario');	
+		}
+		
+		public function new_user() {
+			$this->form_validation->set_rules('nombre', 'Nombre', 'trim|required|min_length[2]|max_length[30]|alpha|xss_clean');
+			$this->form_validation->set_rules('apellido', 'Apellido', 'trim|required|min_length[2]|max_length[30]|alpha');
+			$this->form_validation->set_rules('password', 'Contrase&ntilde;a', 'required|min_length[8]|max_length[30]|md5');
+			$this->form_validation->set_rules('passconf', 'Confirme su contrase&ntilde;a', 'required|matches[password]|md5');
+			$this->form_validation->set_rules('email', 'E-Mail', 'trim|required|valid_email|max_length[50]|callback_username_check');
+			$this->form_validation->set_rules('direccion', 'Direcci&oacute;n', 'trim|required|min_length[2]|max_length[50]|alpha_numeric_spaces');
+			$this->form_validation->set_rules('numero_calle', 'N&uacute;mero de Direcci&oacute;n', 'trim|required|min_length[1]|max_length[10]|is_natural_no_zero');
+			$this->form_validation->set_rules('localidad', 'Localidad', 'trim|required|min_length[2]|max_length[50]|alpha_numeric_spaces');
+			
+			if ($this->form_validation->run() == FALSE) {
+				$this->load->view('registrousuario');
+			} else {			/* VALIDACIONES */
+				if ($this->registro_model->insert_newUser($this->input->post('nombre'), $this->input->post('apellido'), $this->input->post('password'), $this->input->post('email'), $this->input->post('direccion'), $this->input->post('calle_numero'), $this->input->post('localidad'))) {
+					$this->load->view('registrocorrecto');
+					$this->load->library('session');
+				} else {
+					$this->load->view('registroErr');
+				}	
+			}
+		}
+		
+		// COMPROBACION DE E-MAIL, QUE NO SE DUPLIQUE EN LA BASE
+		public function username_check($str) {
+			$exist = $this->registro_model->CheckIfExist($this->input->post('email'));
+			
+			if ($exist) {
+				$this->form_validation->set_message('username_check', 'El %s ya se encuentra en uso');
+				return FALSE;
+			}
+			else
+				return TRUE;
 		}
 	}
-	
-	// COMPROBACION DE E-MAIL, QUE NO SE DUPLIQUE EN LA BASE
-	function username_check($str)
-	{
-		$exist = $this->registro_model->CheckIfExist($this->input->post('email'));
-		
-		if ($exist){
-			$this->form_validation->set_message('username_check', 'El %s ya se encuentra en uso');
-			return FALSE;
-		}
-		else
-			return TRUE;
-	}
-}
 ?>
