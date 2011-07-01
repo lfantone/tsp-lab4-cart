593d20e6d4eda4cad6e2db562c3f0cfa731b8bce
Luciano Fantone <lucianoafantone@gmail.com>
2011-06-30 22:29:33 -0300

    DOCTYPE html PUBLIC

--- application/views/index.php
+++ application/views/index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">  
-<html xml:lang="en-us" xmlns="http://www.w3.org/1999/xhtml">  
+<html>  
 <head>  
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
 	<title>Carrito de compra</title>  
  	<link href="<?=base_url()?>assets/css/core.css" rel="stylesheet" type="text/css" />
  	<link href="<?=base_url()?>assets/css/banner.css" rel="stylesheet"  type="text/css" />  
-	<script type="text/javascript" src="http://jqueryjs.googlecode.com/files/jquery-1.3.2.min.js"></script>
+	<script type="text/javascript" src="<?=base_url()?>/assets/js/jquery-1.6.1.min.js"></script>
 	<script type="text/javascript" src="<?=base_url()?>/assets/js/core.js"></script>  
 </head>  
 <body>
