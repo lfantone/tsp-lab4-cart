598392333ff26e4b0acd538686f8848f92f36aa6
Luciano Fantone <lucianoafantone@gmail.com>
2011-07-01 13:29:15 -0300

    cambios menores

--- application/views/banner.php
+++ application/views/banner.php
@@ -1,10 +1,11 @@
-	<div id="dolphincontainer">
-	  <div id="dolphinnav">
-	  <ul>
-	    <li><?php echo anchor('cart', 'Home'); ?></li>
-		<li><?php echo anchor('cart', 'Productos'); ?></li>
-		<li><?php echo anchor('perfil', 'Mi perfil'); ?></li>
-		<li><?php echo anchor('cart', 'Mi carrito'); ?></li>
-	  </ul>
-	  </div>
+	<div id="dolphincontainer">		
+		<div id="dolphinnav">
+	  		<ul>
+	    		<li><?php echo anchor('main', 'Home'); ?></li>
+				<li><?php echo anchor('productos', 'Productos'); ?></li>
+				<li><?php echo anchor('perfil', 'Mi perfil'); ?></li>
+				<li><?php echo anchor('carro', 'Mi carrito'); ?></li>
+				<li><?php echo anchor('logout', 'Log Out'); ?></li>
+			</ul>
+	  	</div>
 	</div>
--- application/views/banner_admin.php
+++ application/views/banner_admin.php
@@ -2,7 +2,7 @@
 	  <div id="dolphinnav">
 	  <ul>
 	    <li><?php echo anchor('cpanel', 'Home'); ?></li>
-		<li><?php echo anchor('exit', 'Log Out'); ?></li>		
+		<li><?php echo anchor('logout', 'Log Out'); ?></li>		
 	  </ul>
 	  </div>
 	</div>
